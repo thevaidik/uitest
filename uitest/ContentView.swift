5347b049e002e6cde4e1bf11daa1f5fc9b0895cc
thevaidik <vaidik50000@gmail.com>
2023-12-09 17:51:26 +0530

    Initial Commit

---
+++ uitest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by Vaidik Dubey on 09/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Vaidik Dubey on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
